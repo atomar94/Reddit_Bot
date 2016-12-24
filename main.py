7343a948ac3f61f505713183317faf3a16f0388e
= <omar112694@gmail.com>
2016-12-23 17:53:15 -0800

    * Can now search for text in comments * Can now read all comments in a submission

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import praw
 import configloader
 
 
+#Given a submission object return a list of comment text.
+def load_comments(submission):
+	submission.comments.replace_more(limit=0)
+	return [x.body for x in submission.comments.list()]
+
+
 if __name__ == "__main__":
 	cfg = configloader.ConfigLoader("credentials.ini")
 
@@ -9,5 +15,14 @@ if __name__ == "__main__":
 					client_secret = cfg.client_secret,
 					user_agent = cfg.user_agent)
 
-	for submission in r.subreddit("askreddit").hot(limit=5):
-		print(submission.title)
+	for submission in r.subreddit("askreddit").hot(limit=2):
+		comment_list = load_comments(submission)
+		for comment in comment_list:
+			if "reddit" in comment:
+				try:
+					print(comment)
+				#because python doesnt like windows unicode translation
+				except UnicodeEncodeError:
+					print("a comment could not displayed.")
+				input()
+
